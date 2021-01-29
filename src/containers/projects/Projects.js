b1bbde462ec37cdcd4bdbee03ecba6339776ee02
Hrishi <hrishipatel99@gmail.com>
2021-01-29 22:10:17 +0530

    Update SVGs and minor improvements

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Project.css";
-import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
+import ProjectCard from "../../components/ProjectCard/";
 
 export default function Projects() {
   const [repo, _] = useState([]);
@@ -10,7 +10,7 @@ export default function Projects() {
       <h1 className="project-title">Open Source Projects</h1>
       <div className="repo-cards-div-main">
         {repo.map((v, i) => {
-          return <GithubRepoCard repo={v} key={v.node.id} />;
+          return <ProjectCard repo={v} key={v.node.id} />;
         })}
       </div>
       <a className="resume-btn" href="https://github.com/hrishi1999">
