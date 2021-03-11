285426287d45e1966ef3dc140cbca260be636d30
Hrishi <hrishipatel99@gmail.com>
2021-03-11 21:38:25 +0530

    update DegreeCard, code cleanup

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -34,19 +34,6 @@ export default function ProjectCard({ repo, theme }) {
           style={{ backgroundColor: theme.projectCard }}
         >
           <div className="repo-name-div">
-            <svg
-              aria-hidden="true"
-              className="octicon repo-svg"
-              height="16"
-              role="img"
-              viewBox="0 0 12 16"
-              width="12"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
-              ></path>
-            </svg>
             <p className="repo-name" style={{ color: theme.text }}>
               {repo.name}
             </p>
@@ -55,10 +42,7 @@ export default function ProjectCard({ repo, theme }) {
             {repo.description}
           </p>
           <div className="repo-details">
-            <ProjectLanguages
-              className="repo-languages"
-              logos={repo.languages}
-            />
+            <ProjectLanguages logos={repo.languages} />
           </div>
         </div>
       </Fade>
--- src/components/degreeCard/DegreeCard.js
+++ src/components/degreeCard/DegreeCard.js
@@ -1,14 +1,76 @@
 import React from "react";
 import "./DegreeCard.css";
 import { Fade, Flip } from "react-reveal";
+import { style } from "glamor";
 
 function DegreeCard(props) {
   const degree = props.degree;
   const theme = props.theme;
+
+  const style_img = style({
+    width: "220px",
+    height: "auto",
+    borderRadius: " 50%",
+    padding: "10px",
+    border: `1px solid ${theme.accentColor}`,
+    marginRight: "50px",
+    boxShadow: `0px 0px 5px ${theme.accentColor}`,
+    transition: "all 0.2s ease-in-out",
+    ":hover": {
+      color: "rgba(255, 255, 255, 1)",
+      boxShadow: `0 5px 15px ${theme.accentColor}`,
+    },
+    "@media (max-width: 768px)": {
+      marginLeft: "50px",
+      marginBottom: "15px",
+      width: "175px",
+    },
+  });
+
+  const card_body = style({
+    borderBottom: `1px solid ${theme.accentColor}`,
+    borderLeft: `1px solid ${theme.accentColor}`,
+    borderRight: `1px solid ${theme.accentColor}`,
+    borderRadius: "7px",
+    width: "90%",
+    margin: "10px",
+    boxShadow: `0px 1px 5px ${theme.accentColor}`,
+    transition: "all 0.2s ease-in-out",
+    ":hover": {
+      color: "rgba(255, 255, 255, 1)",
+      boxShadow: `0 5px 15px ${theme.accentColor}`,
+    },
+    "@media (max-width: 768px)": {
+      width: "100%",
+    },
+  });
+
+  const button_visit = style({
+    textDecoration: "none",
+    color: "rgba(255, 255, 255, 1)",
+    background: `${theme.accentColor}`,
+    padding: "15px 15px",
+    marginTop: "25px",
+    borderRadius: "4px",
+    borderWidth: "0px",
+    marginBottom: "20px",
+    width: "200px",
+    height: "50px",
+    fontWeight: "bold",
+    fontFamily: "Google Sans Regular",
+    fontSize: "17px",
+    transition: "all 0.2s ease-in-out",
+    cursor: "pointer",
+    ":hover": {
+      color: "rgba(255, 255, 255, 1)",
+      boxShadow: `0 5px 10px ${theme.accentColor}`,
+    },
+  });
+
   return (
     <div className="degree-card">
       <Flip left duration={2000}>
-        <div className="card-img">
+        <div {...style_img}>
           <img
             style={{
               maxWidth: "100%",
@@ -21,8 +83,11 @@ function DegreeCard(props) {
         </div>
       </Flip>
       <Fade right duration={2000} distance="40px">
-        <div className="card-body">
-          <div className="body-header" style={{ backgroundColor: "#234282" }}>
+        <div {...card_body}>
+          <div
+            className="body-header"
+            style={{ backgroundColor: theme.accentColor }}
+          >
             <div className="body-header-title">
               <h2 className="card-title" style={{ color: "#FFFFFF" }}>
                 {degree.title}
@@ -52,12 +117,12 @@ function DegreeCard(props) {
               style={{ textDecoration: "none", textAlign: "center" }}
             >
               <p
-                className="button-visit"
+                {...button_visit}
                 style={{
                   marginRight: "23px",
                   textDecoration: "none",
                   float: "right",
-                  backgroundColor: "#234282",
+                  backgroundColor: theme.accentColor,
                 }}
               >
                 Visit Website
