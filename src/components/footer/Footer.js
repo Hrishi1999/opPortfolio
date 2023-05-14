5700f6010d8ed1d25b7fc3fcc030c2528cfef504
Harshil <hr830930@dal.ca>
2023-05-14 15:00:46 -0300

    Removed unnecessary lines

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
-import { Fade } from "react-reveal";
-import { greeting } from "../../portfolio.js";
+// import { Fade } from "react-reveal";
+// import { greeting } from "../../portfolio.js";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export default function Footer(props) {
