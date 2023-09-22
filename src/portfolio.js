b6554fe3187e92bbe66db5c782e0086f5eb41ca1
Harshil Shah <sharshil1299@gmail.com>
2023-09-21 23:44:03 -0300

    removed unnecessary items

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,7 +4,7 @@
 const settings = {
   isSplash: false, // Change this to true if you want to use the splash screen.
   useCustomCursor: true, // Change this to false if you want the good'ol cursor
-  googleTrackingID: "UA-174238252-2",
+  googleTrackingID: "UA-XXXXXXXXX-X",
 };
 
 //Home Page
